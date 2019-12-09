5728e2fd6269f22c6c92e75fda93c0fe1bc2fd5e
Jones Ogolo <jay.topher11@gmail.com>
2019-12-09 16:29:57 +0100

    created a mongoose schema for users

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+
 interface UserSchema extends mongoose.Document {
   firstName: string;
   lastName: string;
@@ -11,3 +13,20 @@ interface UserSchema extends mongoose.Document {
   updatedAt: Date;
   deletedAt: Date;
 }
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: String,
+    lastName: String,
+    userName: { type: String, index: true },
+    email: String,
+    phoneNumber: String,
+    password: String,
+    dob: String,
+    nin: String,
+    deletedAt: { type: Date, default: null },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model<UserSchema>('users', userSchema);

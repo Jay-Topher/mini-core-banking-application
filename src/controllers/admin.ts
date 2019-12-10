a4f3947aaa28f48f56fa558a7b29db66ebbdefb9
Jones Ogolo <jay.topher11@gmail.com>
2019-12-10 13:33:11 +0100

    created a login controller for admin

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -1,5 +1,7 @@
 import Admin from '../models/admin';
 import { iAdmin } from '../../types';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 // Add an admin
 export async function addAdmin(adminObj: iAdmin) {
@@ -14,4 +16,27 @@ export async function addAdmin(adminObj: iAdmin) {
   const admin = new Admin(adminObj);
 
   return admin.save();
+}
+
+export async function loginAdmin(username: string, password: string) {
+  const adminExists = await Admin.findOne({ username: username });
+  if (!adminExists) {
+    throw Error('Invalid username or password');
+  }
+
+  const comparePassword = await bcrypt.compare(password, adminExists.password);
+  if (!comparePassword) {
+    throw new Error('Invalid username or password ');
+  }
+  const token = jwt.sign(
+    { userId: adminExists._id },
+    process.env.ACCESS_TOKEN_SECRET!,
+    { expiresIn: '30m' },
+  );
+  return {
+    status: 200,
+    message: 'Login Succesful',
+    payload: adminExists,
+    token,
+  };
 }

71df00fd2f12d95676cca760297086daf2559302
Jones Ogolo <jay.topher11@gmail.com>
2019-12-10 23:29:31 +0100

    created a route to get single user and to get all users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { addUser } from '../controllers/users';
+import { addUser, getUser, getAllUsers } from '../controllers/users';
 import { iUser, iAccount } from '../../types';
 import { addBvn } from '../controllers/bvn';
 import { openAccount } from '../controllers/accounts';
@@ -72,6 +72,42 @@ router.post('/admin/login', async (req, res) => {
     res.status(status).json({ message, payload, token });
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+});
+
+// get single user
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const doc = await getUser(id);
+
+    if (!doc) {
+      res.status(400).json({ message: 'User not found' });
+
+      return;
+    }
+
+    res.status(200).json(doc);
+    return;
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// get all users
+router.get('/', async (_req, res) => {
+  try {
+    const doc = await getAllUsers();
+
+    if (!doc) {
+      res.status(404).json({ message: 'users not found' });
+
+      return;
+    }
+
+    return res.status(200).json({ doc });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 });
 

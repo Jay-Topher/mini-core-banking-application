168ac462fe6928eede8ee2a7fba0796ae3aa8630
Jones Ogolo <jay.topher11@gmail.com>
2019-12-10 18:59:15 +0100

    created routes for the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import Home from './views/Home/Home';
+import SignUpForm from './views/Signup/SignupForm';
+import SignUp from './views/Signup/SignUp';
+import LoginPage from './views/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/login" component={LoginPage} />
+      {/* <Route path="/profile" component={Dashboard} />
+      <Route path="/tasks" component={Dashboard} />
+      <Route path="/team" component={Dashboard} /> */}
+      {/* <Route path="/confirm/:id" component={Confirm} /> */}
+    </Switch>
   );
 }
 

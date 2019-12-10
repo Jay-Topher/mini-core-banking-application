da03d9e70d330d5e4d4d51ba6a5a64e65b9a9eca
Jones Ogolo <jay.topher11@gmail.com>
2019-12-10 01:48:00 +0100

    created a controller for the user

---
+++ src/controllers/users.ts
@@ -0,0 +1,23 @@
+import Users from '../models/users';
+import { iUser } from '../../types';
+
+export async function openAccount(userObj: iUser) {
+  const existingUser = await Users.find({
+    email: userObj.email,
+    userName: userObj.userName,
+  });
+
+  if (existingUser.length !== 0) {
+    throw Error(
+      `User with username ${userObj.userName} or email ${userObj.email} already exists.`,
+    );
+  }
+
+  const user = new Users(userObj);
+
+  return user.save();
+}
+
+export async function closeAccount(id: string) {
+  return Users.findOneAndUpdate({ _id: id }, { deletedAt: new Date() });
+}
